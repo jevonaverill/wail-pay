3e163e0dca78c46a97a32ac5272dd44c3b09743d
Jevon Averill <jevonaverill@Jevons-MacBook-Pro.local>
2018-02-11 23:50:28 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/javerill/wailpay/DetailActivity.java
@@ -0,0 +1,25 @@
+package com.example.javerill.wailpay;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by javerill on 9/30/2016.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+//        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        // Set Collapsing Toolbar layout to the screen
+//        CollapsingToolbarLayout collapsingToolbar =
+//                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+//        // Set title of Detail page
+//        collapsingToolbar.setTitle(getString(R.string.app_name));
+    }
+
+}
